e45c3a8f402ae4701c5d0b376a8bae1f02d913dd
Ruhi Atakan Bekmezci <atakanbekmezci@hotmail.com>
2021-05-09 16:38:48 +0300

    ilkduzenleme

--- nortwind/src/main/java/kodlamaio/nortwind/business/abstracts/ProductService.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.nortwind.business.abstracts;
-
-import kodlamaio.nortwind.entities.concretes.Product;
-
-import java.util.List;
-
-public interface ProductService {
-    List<Product> getAll();
-}
